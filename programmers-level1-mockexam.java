ae9b8aaf84c70e23ed3283a9fde782d12bd11eb2
yule93 <dbfm2222@gmail.com>
2020-09-26 14:57:07 +0900

    몇몇 문제들 푼 거
    programmers 문제 위주로

---
+++ programmers-level1-mockexam.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class Solution {
+	public int[] solution(int[] answers) {
+		int[] answer = {};
+		int[] person1 = {1, 2, 3, 4, 5};
+		int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+        int answer1=0, answer2 =0, answer3 =0;
+	}
+}
---
+++ programmers-level2-tuple.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+		String[] strs = s.replaceAll ("[{}]", " ").trim().split(" ,");	// trim으로 공백 제거, split으로 문자열을 나눠 문자 배열 생성
+		answer = new int [strs.length];
+		HashSet<Integer> hs = new HashSet<Integer>(); // Set을 쓰는 이유는 보통 중복 제거
+		Arrays.sort(strs, (a,b)->(a.length()-b.length()));
+		int i = 0;
+		for(String str: strs) {
+			for(String strrr: str.split(",")) {
+				int a = Integer.parseInt(strrr.trim());
+				if(hs.contains(a)) continue;
+				hs.add(a);
+				answer[i++] = a;
+			}
+		}
+        return answer;
+    }
+}
