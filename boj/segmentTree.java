11135972e16e8f45b2df2022dec1f188ed715401
yule93 <dbfm2222@gmail.com>
2021-05-10 23:23:01 +0900

    modify segment update function

--- boj/segmentTree.java
+++ boj/segmentTree.java
@@ -20,5 +20,14 @@ public class segmentTree {
         return sum(start, mid, left, right, node*2) + sum(mid + 1, end, left, right, node*2+1);
     }
 
-    void vector() {}
+    void update(int start, int end, int index, long diff, int node) {
+        if(index < start || index > end) return;
+        tree[node] = diff;      // tree[node] + diff 방법도 있긴 한데 왜 굳이 diff를 val-a[index]로 정한건지 아리송....
+        int mid = (start + end) / 2;
+
+        if(start != end) {
+            update(start, mid, index, diff, node*2);
+            update(mid+1, end, index, diff, node*2+1);
+        }
+    }
 }
