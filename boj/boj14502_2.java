0a8c542d85f280aa41488f830ebb8979b4653ed0
송민주 <dbfm2222@naver.com>
2021-09-17 11:15:48 +0900

    resolve boj 14502 BFS

--- boj/boj14502_2.java
+++ boj/boj14502_2.java
@@ -1,16 +1,19 @@
 package boj;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class boj14502 {
+public class boj14502_2 {
     static int N, M, res = 0;
     static int[][] map, infectedMap;
-    static int[] dy = { -1, 0, 1, 0 }, dx = { 0, -1, 0, 1 };
+    static int[] dy = { -1, 0, 1, 0 }
+    static int[] dx = { 0, -1, 0, 1 };
     static Queue<int[]> q;
     static ArrayList<int[]> virus;
 
@@ -36,7 +39,9 @@ public class boj14502 {
     }
 
     public static void setupWall(int cnt) {
+        // 벽을 세 개 모두 설치했다면
         if (cnt == 3) {
+            // 바이러스를 퍼트림.
             spreadVirus();
             return;
         }
@@ -52,30 +57,38 @@ public class boj14502 {
     }
 
     public static void spreadVirus() {
+        // 원본 보존을 위해 현재 map을 복사
         infectedMap = new int[N][M];
         for (int i = 0; i < N; i++)
             System.arraycopy(map[i], 0, infectedMap[i], 0, M);
 
         q = new LinkedList<int[]>();
+        // 바이러스의 좌표들을 하나씩 확인하면서
         for (int i = 0; i < virus.size(); i++) {
             int[] v = virus.get(i);
+            // Queue에 넣어주고
             q.add(new int[] { v[0], v[1] });
+            // 바이러스를 퍼뜨려보자.
             while (!q.isEmpty()) {
                 int[] now = q.poll();
 
                 for (int d = 0; d < 4; d++) {
                     int ny = now[0] + dy[d];
                     int nx = now[1] + dx[d];
+                    // 범위를 벗어나면 pass
                     if (ny < 0 || nx < 0 || ny >= N || nx >= M)
                         continue;
+                    // 벽이거나 이미 감염된 곳이면 pass
                     if (infectedMap[ny][nx] == 1 || infectedMap[ny][nx] == 3)
                         continue;
+                    // 빈 칸이면 바이러스로 감염시키고 Queue에 add
+                    // visited대신 감염된 곳은 3으로
                     infectedMap[ny][nx] = 3;
                     q.add(new int[] { ny, nx });
                 }
             }
         }
-
+        // 바이러스가 모두 퍼졌을 때, 안전 영역의 크기를 count
         int sum = 0;
         for (int x = 0; x < N; x++) {
             for (int y = 0; y < M; y++) {
@@ -83,7 +96,6 @@ public class boj14502 {
                     sum += 1;
             }
         }
-
         res = Math.max(res, sum);
-    } // end of main
-} // end of class
+    }
+}
